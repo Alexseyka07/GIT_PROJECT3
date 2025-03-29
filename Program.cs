7a1d5aed12a8c0e6230780dfd4577f558876c3a5
Alexseyka07 <alex07progtime@gmail.com>
2025-03-29 19:39:46 +0300

    upd Welcome

--- Program.cs
+++ Program.cs
@@ -5,8 +5,8 @@
         Console.WriteLine("Я меня");
     }
 
-    private static void Welcome(){
-        System.Console.WriteLine("Hi");
+    private static void Welcome(string name){
+        System.Console.WriteLine($"Hi, {name}");
     }
 
     private static int Sum(int a, int b){
